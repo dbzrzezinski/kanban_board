7e45548ab3cafb7f9675773943d84b7cff9818a9
Daniel Brzezinski <danielb.wiesbaden@gmail.com>
2023-03-14 07:15:18 +0100

    Create initial Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Layout from "./components/Layout/Layout";
 import { ApiMockResponse as BoardDataMock } from "./ApiMockData/dummyData";
 import { useEffect, useState } from "react";
@@ -7,6 +6,7 @@ import Lane from "./components/Lane/Lane";
 import { DragDropContext, Droppable, DropResult } from "@hello-pangea/dnd";
 import { Board, Item } from "./Types/KanbanBoard.types";
 import { canUseDOM } from "./Helper/Dom";
+import { Modal } from "./components/Modal/Modal";
 
 function App() {
   const [boardData, setBoardData] = useState<Board[]>([]);
@@ -14,6 +14,7 @@ function App() {
   const [showBoard, setShowBoard] = useState<boolean>(true);
   const [showAddTaskFormLane, setShowAddTaskFormLane] = useState<number>(-1);
   const [filteredBoardData, setFilteredBoardData] = useState<Board[]>();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 
   const STORAGE_KEY_BOARD_DATA = "kanban_board";
 
@@ -114,80 +115,91 @@ function App() {
   };
 
   return (
-    <div className="min-w-full min-h-screen ">
-      {isReady && (
-        <Layout setSearchValue={handleSearchInputChange}>
-          <div className="p-10">
-            <div className="flex flex-initial justify-between flex-col lg:flex-row">
-              <div className="flex items-center justify-center md:justify-start">
-                <h4 className="text-3xl font-bold text-gray-600">
-                  Kanban Board
-                </h4>
-                <button onClick={() => setShowBoard(!showBoard)}>
-                  <ChevronUpIcon
-                    className={`${
-                      showBoard ? "rotate-180" : ""
-                    } w-7 h-7 text-gray-600 rounded-full bg-white p-2 ml-2 mt-1 hover:cursor-pointer`}
-                  />
-                </button>
-              </div>
-
-              <div className="mt-8 lg:mt-0 ">
-                <ul className="flex space-x-3 justify-center md:justify-start">
-                  {/* Show some random ids/user portraits */}
-                  {users.map((person, index) => (
-                    <li key={person.id}>
-                      <img
-                        src={`https://randomuser.me/api/portraits/men/${person.id}.jpg`}
-                        width="32"
-                        height="32"
-                        className="object-cover rounded-full"
-                        title={person.name}
-                      />
-                    </li>
-                  ))}
-                  <li>
-                    <button className="border border-dashed rounded-full flex items-center justify-center w-8 h-8 border-gray-500 hover:text-gray-700 hover:border-solid hover:shadow-md opacity-70 hover:opacity-100">
-                      <PlusIcon className="w-6 h-6 text-gray-500 hover:text-gray-700" />
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <DragDropContext onDragEnd={onDragEnd}>
-              {showBoard && (
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-5 my-5">
-                  {filteredBoardData &&
-                    filteredBoardData.map((laneData: Board, index: number) => (
-                      <Droppable droppableId={index.toString()} key={index}>
-                        {(provided) => (
-                          <div>
-                            <div
-                              {...provided.droppableProps}
-                              ref={provided.innerRef}
-                            >
-                              <Lane
-                                laneData={laneData}
-                                droppableProvider={provided}
-                                showAddTaskFormLane={showAddTaskFormLane}
-                                submitNewTask={handleSubmitNewTask}
-                                setShowAddTaskFormLane={setShowAddTaskFormLane}
-                                laneIndex={index}
-                              />
-                            </div>
-                            {provided.placeholder}
-                          </div>
-                        )}
-                      </Droppable>
-                    ))}
+    <>
+      <div className="min-w-full min-h-screen" id="App">
+        {isReady && (
+          <Layout setSearchValue={handleSearchInputChange}>
+            <div className="p-10">
+              <div className="flex flex-initial justify-between flex-col lg:flex-row">
+                <div className="flex items-center justify-center md:justify-start">
+                  <h4 className="text-3xl font-bold text-gray-600">
+                    Kanban Board
+                  </h4>
+                  <button onClick={() => setShowBoard(!showBoard)}>
+                    <ChevronUpIcon
+                      className={`${
+                        showBoard ? "rotate-180" : ""
+                      } w-7 h-7 text-gray-600 rounded-full bg-white p-2 ml-2 mt-1 hover:cursor-pointer`}
+                    />
+                  </button>
                 </div>
-              )}
-            </DragDropContext>
-          </div>
-        </Layout>
+
+                <div className="mt-8 lg:mt-0 ">
+                  <ul className="flex space-x-3 justify-center md:justify-start">
+                    {/* Show some random ids/user portraits */}
+                    {users.map((person, index) => (
+                      <li key={person.id}>
+                        <img
+                          src={`https://randomuser.me/api/portraits/men/${person.id}.jpg`}
+                          width="32"
+                          height="32"
+                          className="object-cover rounded-full"
+                          title={person.name}
+                        />
+                      </li>
+                    ))}
+                    <li>
+                      <button className="border border-dashed rounded-full flex items-center justify-center w-8 h-8 border-gray-500 hover:text-gray-700 hover:border-solid hover:shadow-md opacity-70 hover:opacity-100">
+                        <PlusIcon className="w-6 h-6 text-gray-500 hover:text-gray-700" />
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <DragDropContext onDragEnd={onDragEnd}>
+                {showBoard && (
+                  <div className="grid grid-cols-1 md:grid-cols-4 gap-5 my-5">
+                    {filteredBoardData &&
+                      filteredBoardData.map(
+                        (laneData: Board, index: number) => (
+                          <Droppable droppableId={index.toString()} key={index}>
+                            {(provided) => (
+                              <div>
+                                <div
+                                  {...provided.droppableProps}
+                                  ref={provided.innerRef}
+                                >
+                                  <Lane
+                                    laneData={laneData}
+                                    droppableProvider={provided}
+                                    showAddTaskFormLane={showAddTaskFormLane}
+                                    submitNewTask={handleSubmitNewTask}
+                                    setShowAddTaskFormLane={
+                                      setShowAddTaskFormLane
+                                    }
+                                    laneIndex={index}
+                                  />
+                                </div>
+                                {provided.placeholder}
+                              </div>
+                            )}
+                          </Droppable>
+                        )
+                      )}
+                  </div>
+                )}
+              </DragDropContext>
+            </div>
+          </Layout>
+        )}
+      </div>
+      {isModalOpen && (
+        <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+          Content
+        </Modal>
       )}
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,51 @@
+import { useState, Dispatch, SetStateAction, ReactNode } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+type ModalProps = {
+  isModalOpen: boolean;
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+  children: ReactNode;
+  headline?: ReactNode;
+  footer?: ReactNode;
+};
+
+const Modal = ({
+  isModalOpen,
+  setIsModalOpen,
+  children,
+  headline,
+  footer,
+}: ModalProps) => {
+  const handleClose = (e: any) => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <div className="fixed inset-0 z-20 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="flex item-center justify-between p-4 text-xl font-semibold text-gray-900 dark:text-white w-full">
+              <div>{headline}</div>
+              <XMarkIcon
+                className="h-6 w-6 text-gray-400 hover:cursor-pointer hover:text-gray-600"
+                title="Close"
+                onClick={handleClose}
+              />
+            </div>
+            <div className="p-4 w-full">{children}</div>
+            {footer && (
+              <>
+                <div className="flex items-center p-4 bg-gray-50 w-full">
+                  Modal Footer
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="fixed z-10 inset-0 bg-opacity-75 bg-gray-500"></div>
+    </>
+  );
+};
+
+export { Modal };
